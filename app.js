230fd1a6b4b41bc44f910185d3359f8f780643ef
MacRat <m@crat.jp>
2017-08-04 00:38:46 +0900

    Initial

---
+++ app.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 5000;
+const DATABASE = process.env.DATABASE || './db.sqlite';
+
+
+const app = require('express')();
+const server = require('http').Server(app);
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database(DATABASE);
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.get('/api/temphumid', (req, res) => {
+	db.all('SELECT timestamp, temp, humid FROM temphumid ORDER BY timestamp DESC LIMIT 12', (err, rows) => {
+		if (err) throw err;
+
+		res.contentType('application/json');
+		res.send(JSON.stringify({
+			error: null,
+			data: rows
+		}));
+	});
+});
+
+
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
