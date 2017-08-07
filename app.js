78ca69093302bd1486b421e09d47b8629d0c9aca
MacRat <m@crat.jp>
2017-08-07 23:09:43 +0900

    Added graph of temperature and humidity

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const db = new sqlite3.Database(DATABASE);
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 app.get('/api/temphumid', (req, res) => {
-	db.all('SELECT timestamp, temperature AS temp, humidity AS humid FROM temphumid ORDER BY timestamp DESC LIMIT 12', (err, rows) => {
+	var limit = Math.min(3*12, req.query.limit || 12);
+
+	db.all('SELECT timestamp, temperature AS temp, humidity AS humid FROM temphumid ORDER BY timestamp DESC LIMIT ?', limit, (err, rows) => {
 		if (err) throw err;
 
 		res.contentType('application/json');
